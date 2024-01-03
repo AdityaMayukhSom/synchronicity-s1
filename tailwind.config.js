15576892b3790e0ae0a42a0c8ddb6631d2cc824d
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-03 20:04:32 +0530

    added new font size for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/***/**/*.{js,jsx,ts,tsx}"],
+  content: [ "./index.html", "./src/***/**/*.{js,jsx,ts,tsx}" ],
   theme: {
     extend: {
       colors: {
@@ -53,6 +53,9 @@ export default {
           900: "#303616",
         },
       },
+      fontSize: {
+        '4.5xl': '42px'
+      }
     },
   },
   plugins: [],
